0f7239ca2516aef1f0782772697021fd8b41e35d
Luana da Rosa <lrosa.silva@outlook.com>
2021-12-18 17:45:49 -0300

    teste unitario com mockito

---
+++ src/test/java/HelloWorldMockito.java
@@ -0,0 +1,16 @@
+import br.com.alura.leilao.dao.LeilaoDao;
+import br.com.alura.leilao.model.Leilao;
+import junit.framework.Assert;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+public class HelloWorldMockito {
+    @Test
+    void hello(){
+        LeilaoDao mock = Mockito.mock(LeilaoDao.class);
+        List<Leilao> todos = mock.buscarTodos();
+        Assert.assertTrue(todos.isEmpty());
+    }
+}
